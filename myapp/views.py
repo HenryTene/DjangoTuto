23196c745c6e52306d888c4699fec05d8527d9a7
HenryTene <henry_tene@hotmail.com>
2024-12-17 13:26:34 -0500

    :art: Se modifican metodos para renderizar paginas html

--- myapp/views.py
+++ myapp/views.py
@@ -1,33 +1,35 @@
 from django.http import HttpResponse, JsonResponse
-from .models import Project,Task
+from .models import Project, Task
 from django.shortcuts import get_object_or_404, render, redirect
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("<h1>Index</h1>")
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def projects(request):
+    projects = list(Project.objects.values())
+   ## return JsonResponse(projects, safe=False)
+    return render(request, 'projects.html')
+
+def tasks(request):
+    # task = get_object_or_404(Task)
+    tasks = list(Task.objects.values())
+    #print(tasks)
+    #return JsonResponse(tasks, safe=False)
+    return render(request, 'tasks.html')
+
+def tasksById(request, id):
+    # task = Task.objects.get(id=id)
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse("tasks %s" % task.title)
 
 
 def hello(request, username):
     print(username)
     return HttpResponse("<h1>Hello %s</h1>" % username)
-
-
-def about(request):
-    return HttpResponse("<h2>About</h2>")
-
-
-def projects(request):
-    projects = list(Project.objects.values())
-    return JsonResponse(projects, safe=False)
-
-
-def tasks(request):    
-    #task = get_object_or_404(Task)
-    tasks = list(Task.objects.values())
-    print(tasks)
-    return JsonResponse(tasks, safe=False)
-def tasksById(request, id):
-    #task = Task.objects.get(id=id)
-    task = get_object_or_404(Task, id=id)
-    return HttpResponse("tasks %s" % task.title)
